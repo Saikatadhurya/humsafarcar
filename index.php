904b1ec13a57096e06009af4042f1efc52b65571
SAIKAT ADHURYA <36005394+Saikatadhurya@users.noreply.github.com>
2019-01-02 19:34:49 +0530

    Update index.php

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ $user_id=$_SESSION['uid'];
     <meta name="author" content="">
 
 	<!--Google Map API -->
-	<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC5YcOYeWk0etw76B6HGoR_QvmNAjYsUrs&libraries=places"></script>
+	<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCo8BYxQlKLiQbemkCdn-q5JdVXXS_wJP4&libraries=places"></script>
 	
     <!-- Titles
     ================================================== -->
