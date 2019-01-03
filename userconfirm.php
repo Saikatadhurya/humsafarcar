d5a9b5fb903aca5eea5a6939750f61ee3b4900f6
SAIKAT ADHURYA <36005394+Saikatadhurya@users.noreply.github.com>
2019-01-03 21:48:42 +0530

    Update userconfirm.php

--- userconfirm.php
+++ userconfirm.php
@@ -132,12 +132,36 @@ $row = mysqli_fetch_assoc($query);
 	?>
 	 <div class="row">
 	 <div class="col-md-4">
-	<a href="chat/index.php?did=<?php echo $did?>"><button type="button"  class="btn btn-lg btn-danger">Chat with Driver</button></a>
+	<a href="chat/index.php?did=<?php echo $did?>"><button type="button"  class="btn btn-lg btn-warning">Chat with Driver</button></a>
 	</div>
-	<div class="col-md-4">
-	</div>
+	
 	<div class="col-md-4">
 	<button type="button" class="btn btn-lg btn-success">Confirm Ride</button>
+	</div>
+	<div class="col-md-4">
+	<!-- Button trigger modal -->
+<button type="button" class="btn btn-lg btn-danger" data-toggle="modal" data-target="#exampleModalCenter" style="border-radius:50%">
+  PANIC BUTTON
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title alert-danger" id="exampleModalLongTitle">Panic Button</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary btn-success" data-dismiss="modal">CANCEL</button>
+        <button type="button" class="btn btn-primary btn-danger">ASK FOR HELP</button>
+      </div>
+    </div>
+  </div>
+</div>
 	</div>
 	</div>
 
